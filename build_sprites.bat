3991f2d2d341cda405772f28c126b2e2d217a745
Justin Parker <jparkerweb@gmail.com>
2013-03-19 10:06:43 -0700

    added nzb.ag and nzbindex.in to search providers

--- build_sprites.bat
+++ build_sprites.bat
@@ -1,3 +1,5 @@
-REM 
-glue icons --css=css --img=sprites --padding=2 --html
+@echo off
+glue icons --css=css --img=sprites --padding=1 --margin=2 --html --global-template="%%(all_classes)s{background-image:url('%%(sprite_url)s'); background-repeat:no-repeat; display:inline-block;}\n"
+cls
+echo "Sprite Generated..."
 pause
