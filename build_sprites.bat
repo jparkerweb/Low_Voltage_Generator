ecc6e88ab51b139547a734bdbae6d5c84d5e84c6
Justin Parker <jparkerweb@gmail.com>
2013-04-17 12:00:50 -0700

    cleanup

--- build_sprites.bat
+++ build_sprites.bat
@@ -1,5 +1,5 @@
 @echo off
-glue icons --css=css --img=sprites --padding=1 --margin=2 --html --global-template="%%(all_classes)s{background-image:url('%%(sprite_url)s'); background-repeat:no-repeat; display:inline-block;}\n"
+glue icons --css=css --img=sprites --padding=1 --margin=2 --html --global-template="%%(all_classes)s{background-image:url('%%(sprite_url)s'); background-repeat:no-repeat; display:inline-block; vertical-align:text-top;}\n"
 cls
 echo "Sprite Generated..."
 pause
